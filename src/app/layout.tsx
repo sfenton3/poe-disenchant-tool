591c29c473797ad6910f4d3eb4dac8e178a0be10
Mateusz Dionizy <62315827+deronek@users.noreply.github.com>
2025-10-21 19:49:44 +0200

    Remove Geist font (#123)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,19 +5,8 @@ import { ErrorHandler } from "@/components/error-handler";
 import { ThemeProvider } from "@/components/theme-provider";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "PoE Unique Disenchanting Tool",
@@ -32,9 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="antialiased">
         <TooltipProvider>
           <ThemeProvider
             attribute="class"
