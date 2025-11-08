e8f40bfdef49f34ab0555cad863598cebe4ce055
deronek <dionek3@gmail.com>
2025-11-08 18:32:25 +0100

    Add cheapest catalyst calculation

--- src/lib/prices/index.ts
+++ src/lib/prices/index.ts
@@ -1,4 +1,12 @@
+import type { CatalystItem } from "./currency";
 import type { AllowedUnique, Item } from "./prices";
+import { getCheapestCatalyst } from "./currency";
 import { getPriceData } from "./prices";
 
-export { getPriceData, type Item, type AllowedUnique };
+export {
+  getPriceData,
+  type Item,
+  type AllowedUnique,
+  getCheapestCatalyst,
+  type CatalystItem,
+};
